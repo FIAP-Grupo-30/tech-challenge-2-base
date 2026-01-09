2bbf84730497c20dd10d12f832dac0277efeefc7
Vitor Oliveira <vitorsaop@gmail.com>
2026-01-08 23:31:19 -0300

    fix: import store early so global store is available to webcomponents

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Ensure the Redux store is created and exposed globally before other bundles run
+import './store';
 import App from './App.jsx';
 import './index.css';
 
