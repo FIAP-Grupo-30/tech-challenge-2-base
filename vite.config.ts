be7abbfad6eb8e0ef4276d795ecda16483e323b2
Douglas Gomes <douglasgomes.rr@gmail.com>
2026-01-17 01:59:08 -0300

    feat: update environment variables for remote entry points and enhance package configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,53 @@
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { z } from "zod";
 
-export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		federation({
-			name: "@bytebank/base",
-			filename: "remoteEntry.js",
-			exposes: {
-				"./bytebank-base": "./src/exposes/bytebank-base.tsx",
-			},
-			remotes: {
-				"@bytebank/financeiro": "http://localhost:9002/assets/remoteEntry.js",
-				"@bytebank/dashboard": "http://localhost:9003/assets/remoteEntry.js",
-				"@bytebank/root": "http://localhost:9000/assets/remoteEntry.js",
-			},
-			shared: ["react", "react-dom", "react-router-dom"],
-		}),
-	],
-	build: {
-		modulePreload: false,
-		target: "esnext",
-		minify: false,
-		cssCodeSplit: false,
-	},
-	server: {
-		port: 9000,
-		fs: {
-			allow: ["."],
+const schema = z.object({
+	VITE_API_BASE_URL: z.url(),
+	VITE_REMOTE_ROOT_URL: z.url(),
+	VITE_REMOTE_FINANCEIRO_URL: z.url(),
+	VITE_REMOTE_DASHBOARD_URL: z.url(),
+	VITE_REMOTE_BASE_URL: z.url(),
+});
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	const ENV = schema.parse(loadEnv(mode, process.cwd(), ""));
+
+	return {
+		plugins: [
+			react(),
+			tailwindcss(),
+			federation({
+				name: "@bytebank/base",
+				filename: "remoteEntry.js",
+				exposes: {
+					"./bytebank-base": "./src/exposes/bytebank-base.tsx",
+				},
+				remotes: {
+					"@bytebank/financeiro": ENV.VITE_REMOTE_FINANCEIRO_URL,
+					"@bytebank/dashboard": ENV.VITE_REMOTE_DASHBOARD_URL,
+					"@bytebank/root": ENV.VITE_REMOTE_ROOT_URL,
+				},
+				shared: ["react", "react-dom", "react-router-dom"],
+			}),
+		],
+		build: {
+			modulePreload: false,
+			target: "esnext",
+			minify: false,
+			cssCodeSplit: false,
 		},
-	},
-	preview: {
-		port: 9001,
-	},
+		server: {
+			port: 9000,
+			fs: {
+				allow: ["."],
+			},
+		},
+		preview: {
+			port: 9001,
+		},
+	};
 });
